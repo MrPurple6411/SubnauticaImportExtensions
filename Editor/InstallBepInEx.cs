de42d675a613d5f39d380911b4781fb2cfbd765a
MrPurple6411 <mrpurple6411@gmail.com>
2023-09-23 01:37:08 -0600

    Update InstallBepInEx.cs

--- Editor/InstallBepInEx.cs
+++ Editor/InstallBepInEx.cs
@@ -5,8 +5,8 @@
     public class InstallBepInEx : ThunderstorePackageInstaller
     {
         public override int Priority => Constants.Priority.InstallBepInEx;
-        public override string ThunderstoreAddress => "https://thunderstore.io";
-        public override string DependencyId => "bbepis-BepInExPack";
+        public override string ThunderstoreAddress => "https://subnautica.thunderstore.io";
+        public override string DependencyId => "Subnautica_Modding-BepInExPack";
         public override string Description => $"Installs the latest version of BepInEx.\r\nThe Unified BepInEx all-in-one modding pack - plugin framework, detour library";
         public override string Name => $"Install BepInEx";
     }
